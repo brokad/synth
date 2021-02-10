d3aac2bdd7f0323717a75bba5304c147f3eed56b
Damien Broka <damienbroka@mailbox.org>
2021-02-10 17:22:31 +0000

    WIP

--- tools/install.nix
+++ tools/install.nix
@@ -1,7 +1,10 @@
+{
+  ref ? "feat/dockerfile"
+, rev ? "11acb9d901646b04354dbf44a6acafec3bbf7d3f"
+}:
 let
   src = builtins.fetchGit {
     url = "https://github.com/brokad/synth.git";
-    ref = "feat/dockerfile";
-    rev = "11acb9d901646b04354dbf44a6acafec3bbf7d3f";
+    inherit ref rev;
   };
 in (import "${src}/nix/release.nix" {}).override { inherit src; }
